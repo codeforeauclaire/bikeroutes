8f91dd15eaa66ced962e6c2ab52aa531e8967a20
Anthony <Anthony123272@gmail.com>
2016-05-09 10:28:42 -0500

    First shot at a vms quickinstall script

---
+++ react/README.md
@@ -0,0 +1,8 @@
+ ## VMS development enviornment setup instructions
+
+ 1. Create a new Digital Ocean Ubuntu 14.04.4 machine >> SSH in >> Run (or read) vms quick install
+  1. `curl -L -o- https://github.com/codeforeauclaire/bikeroutes/tree/react/react/bin/vmsquickinstall.sh | bash`
+  1. Run app
+   1. `(cd ~/bikeroutes/react && ./bin/run-dev-server)`
+   1. Load http://{vms-ip}:8080 in your browser
+   1. Edit a .jsx file >> restart ./bin/run-dev-server >> see changes in your browser
---
+++ react/bin/vmsquickinstall.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+{ # this ensures the entire script is downloaded and run #
+
+
+# Update all software & install new from package manager
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y git
+
+## Install Node Version Manager w/recent node version
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+nvm install v6.1.0
+nvm use v6.1.0
+
+# Install webpack & it's dev-server with global access
+npm install webpack -g
+npm install webpack-dev-server -g
+
+# Setup repository
+git clone https://github.com/codeforeauclaire/bikeroutes.git /root/bikeroutes
+(cd /root/bikeroutes && git checkout react)
+
+# Install dependencies from package.json
+(cd /root/bikeroutes/react && npm install --save)
+
+} # this ensures the entire script is downloaded and run #
